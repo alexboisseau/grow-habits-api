827c47a73d9136d7ca41a4ecfc2e800a8b5dbe34
alexboisseau <alex.boisseau79@gmail.com>
2023-10-30 17:50:20 +0100

    task: create a zoz validation pipe

---
+++ src/habits/pipes/zod-validation.pipe.ts
@@ -0,0 +1,14 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { z } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: z.Schema<any>) {}
+
+  transform(payload: any) {
+    const result = this.schema.safeParse(payload);
+
+    if (result.success === true) return result.data;
+    throw new BadRequestException('Failed to validate');
+  }
+}
