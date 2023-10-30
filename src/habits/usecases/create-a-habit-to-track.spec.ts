87ff37f58b7249098547922f4bfc0ab73608660c
alexboisseau <alex.boisseau79@gmail.com>
2023-10-30 16:38:49 +0100

    refactor: create a dedicated function to expect habit to equal a static value

--- src/habits/usecases/create-a-habit-to-track.spec.ts
+++ src/habits/usecases/create-a-habit-to-track.spec.ts
@@ -1,9 +1,23 @@
 import { FixedDateGenerator } from '../adapters/fixed-date-generator';
 import { FixedIdGenerator } from '../adapters/fixed-id-generator';
 import { InMemoryHabitRepository } from '../adapters/in-memory-habit-repository';
+import { Habit } from '../entities/habit.entity';
 import { CreateAHabitToTrack } from './create-a-habit-to-track';
 
 describe('Feature : create a habit to track', () => {
+  function expectHabitToEqual(habit: Habit) {
+    expect(habit.props).toEqual({
+      id: 'id-1',
+      userId: 'bob',
+      trackedFrom: dateGenerator.now(),
+      name: 'Brush my teeth',
+      cue: 'After my breakfast',
+      craving: 'Clean my teeth',
+      response: 'Brush my teeth during three minutes',
+      reward: 'Have a good feeling with fresh breath',
+    });
+  }
+
   let habitRepository: InMemoryHabitRepository;
   let idGenerator: FixedIdGenerator;
   let dateGenerator: FixedDateGenerator;
@@ -33,9 +47,7 @@ describe('Feature : create a habit to track', () => {
     it('should create a new habit and return an id', async () => {
       const response = await useCase.execute(payload);
 
-      expect(response).toEqual({
-        id: 'id-1',
-      });
+      expect(response.id).toEqual('id-1');
     });
 
     it('should save the habit in the persistence layer', async () => {
@@ -43,7 +55,7 @@ describe('Feature : create a habit to track', () => {
 
       const savedHabit = await habitRepository.findById(response.id);
 
-      expect(savedHabit).not.toBeNull();
+      expectHabitToEqual(savedHabit);
     });
 
     it('should add a date field to know from when the user start to track the habit', async () => {
