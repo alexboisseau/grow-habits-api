1ab20f776a2c98a88f96d035f03794e2eeeb8216
alexboisseau <alex.boisseau79@gmail.com>
2023-10-31 15:09:30 +0100

    task: create usecase file with its test file

---
+++ src/habits/usecases/complete-habit.spec.ts
@@ -0,0 +1,9 @@
+import { CompleteHabit } from './complete-habit';
+
+describe('Feature : complete a habit', () => {
+  let useCase: CompleteHabit;
+
+  beforeEach(() => {
+    useCase = new CompleteHabit();
+  });
+});
---
+++ src/habits/usecases/complete-habit.ts
@@ -0,0 +1,5 @@
+export class CompleteHabit {
+  async execute(): Promise<void> {
+    return;
+  }
+}
