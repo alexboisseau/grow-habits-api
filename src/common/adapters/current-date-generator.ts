605e0a16d8b214be57a1c2e84fd43ecd19ad76e7
alexboisseau <alex.boisseau79@gmail.com>
2023-11-01 14:48:48 +0100

    task: add new method in date generator to get current date at midnight

--- src/common/adapters/current-date-generator.ts
+++ src/common/adapters/current-date-generator.ts
@@ -4,4 +4,9 @@ export class CurrentDateGenerator implements IDateGenerator {
   now(): Date {
     return new Date();
   }
+
+  currentDateAtMidnight(): Date {
+    const now = this.now();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  }
 }
--- src/common/adapters/fixed-date-generator.ts
+++ src/common/adapters/fixed-date-generator.ts
@@ -4,4 +4,8 @@ export class FixedDateGenerator implements IDateGenerator {
   now() {
     return new Date('2024-01-01T00:00:00.000Z');
   }
+
+  currentDateAtMidnight(): Date {
+    return new Date('2024-01-01T00:00:00.000Z');
+  }
 }
