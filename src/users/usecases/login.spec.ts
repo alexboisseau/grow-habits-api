ba72df988c005723a80a11fb587674020e18881e
alexboisseau <alex.boisseau79@gmail.com>
2023-11-03 15:41:55 +0100

    task: add case where credentials are not valid

--- src/users/usecases/login.spec.ts
+++ src/users/usecases/login.spec.ts
@@ -1,27 +1,33 @@
+import { FakePasswordHasher } from '../adapters/fake-password-hasher';
 import { InMemorySessionManager } from '../adapters/in-memory-session-manager';
+import { InMemoryUserRepository } from '../adapters/in-memory-user-repository';
 import { User } from '../entities/user.entity';
 import { Login } from './login';
 
 describe('Feature: login', () => {
   const alice = new User({
     id: 'id-1',
-    password: 'Welcome@123',
+    password: 'hashed-Welcome@123',
     email: 'alice@gmail.com',
   });
 
+  let userRepository: InMemoryUserRepository;
+  let passwordHasher: FakePasswordHasher;
   let sessionManager: InMemorySessionManager;
   let useCase: Login;
 
   beforeEach(() => {
+    userRepository = new InMemoryUserRepository([alice]);
+    passwordHasher = new FakePasswordHasher();
     sessionManager = new InMemorySessionManager();
-    useCase = new Login(sessionManager);
+    useCase = new Login(userRepository, passwordHasher, sessionManager);
   });
 
   describe('Happy path', () => {
     it('should create a session with the userId', async () => {
       const payload = {
         email: alice.props.email,
-        password: alice.props.password,
+        password: 'Welcome@123',
       };
 
       const { sessionId } = await useCase.execute({
@@ -29,6 +35,19 @@ describe('Feature: login', () => {
       });
 
       expect(sessionManager.getSessionSync(sessionId)).toEqual(alice.props.id);
+    });
+  });
+
+  describe('Unhappy path : wrong credentials', () => {
+    it('should fail', async () => {
+      const payload = {
+        email: alice.props.email,
+        password: 'random-password',
+      };
+
+      await expect(useCase.execute(payload)).rejects.toThrow(
+        'Invalid credentials',
+      );
     });
   });
 });
--- src/users/usecases/login.ts
+++ src/users/usecases/login.ts
@@ -1,4 +1,8 @@
+import { User } from '../entities/user.entity';
+import { WrongCredentialsException } from '../exceptions/wrong-credentials';
+import { IPasswordHasher } from '../ports/password-hasher.interface';
 import { ISessionManager } from '../ports/session-manager.interface';
+import { IUserRepository } from '../ports/user-repository.interface';
 
 type Request = {
   email: string;
@@ -10,9 +14,22 @@ type Response = {
 };
 
 export class Login {
-  constructor(private readonly sessionManager: ISessionManager) {}
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly passwordHasher: IPasswordHasher,
+    private readonly sessionManager: ISessionManager,
+  ) {}
 
   async execute(request: Request): Promise<Response> {
+    const user = (await this.userRepository.findByEmail(request.email)) as User;
+
+    const passwordIsValid = await this.passwordHasher.compare(
+      request.password,
+      user.props.password,
+    );
+
+    if (!passwordIsValid) throw new WrongCredentialsException();
+
     const sessionId = await this.sessionManager.createSession('id-1');
 
     return {
