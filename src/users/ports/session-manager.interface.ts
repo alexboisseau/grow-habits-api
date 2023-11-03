6906eebec897725c0d73318534033eab914d25da
alexboisseau <alex.boisseau79@gmail.com>
2023-11-03 21:25:43 +0100

    task: add login endpoint to the users module through controller

--- src/users/ports/session-manager.interface.ts
+++ src/users/ports/session-manager.interface.ts
@@ -1,3 +1,4 @@
+export const I_SESSION_MANAGER = 'I_SESSION_MANAGER';
 export interface ISessionManager {
   createSession(userId: string): Promise<string>;
   deleteSession(sessionId: string): Promise<void>;
--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -7,6 +7,9 @@ import { I_ID_GENERATOR } from '../common/ports/id-generator.interface';
 import { FakePasswordHasher } from './adapters/fake-password-hasher';
 import { I_PASSWORD_HASHER } from './ports/password-hasher.interface';
 import { CommonModule } from '../common/common.module';
+import { Login } from './usecases/login';
+import { InMemorySessionManager } from './adapters/in-memory-session-manager';
+import { I_SESSION_MANAGER } from './ports/session-manager.interface';
 
 @Module({
   imports: [CommonModule],
@@ -24,10 +27,28 @@ import { CommonModule } from '../common/common.module';
       },
     },
     {
+      provide: I_SESSION_MANAGER,
+      useFactory: () => {
+        return new InMemorySessionManager();
+      },
+    },
+    {
       provide: Register,
-      inject: [I_USER_REPOSITORY, I_ID_GENERATOR, I_PASSWORD_HASHER],
+      inject: [
+        I_USER_REPOSITORY,
+        I_ID_GENERATOR,
+        I_PASSWORD_HASHER,
+        I_SESSION_MANAGER,
+      ],
       useFactory: (userRepository, idGenerator, passwordHasheur) => {
         return new Register(userRepository, idGenerator, passwordHasheur);
+      },
+    },
+    {
+      provide: Login,
+      inject: [I_USER_REPOSITORY, I_PASSWORD_HASHER, I_SESSION_MANAGER],
+      useFactory: (userRepository, passwordHasheur, sessionManager) => {
+        return new Login(userRepository, passwordHasheur, sessionManager);
       },
     },
   ],
