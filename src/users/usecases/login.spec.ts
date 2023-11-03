266a26d1ae68891be0c1a4d7d08b46138710c2fe
alexboisseau <alex.boisseau79@gmail.com>
2023-11-03 11:10:11 +0100

    task: create login usecase file and test file

---
+++ src/users/usecases/login.spec.ts
@@ -0,0 +1,5 @@
+describe('Feature: login', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/users/usecases/login.ts
@@ -0,0 +1,5 @@
+export class Login {
+  async execute(email: string, password: string) {
+    throw new Error('Not implemented');
+  }
+}
