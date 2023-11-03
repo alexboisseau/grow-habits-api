b7915606d758213ac37cb8e08a188cb941831d12
alexboisseau <alex.boisseau79@gmail.com>
2023-11-03 15:27:10 +0100

    task: add first test to check that a sessionId has been created

--- src/users/usecases/login.spec.ts
+++ src/users/usecases/login.spec.ts
@@ -1,5 +1,34 @@
+import { InMemorySessionManager } from '../adapters/in-memory-session-manager';
+import { User } from '../entities/user.entity';
+import { Login } from './login';
+
 describe('Feature: login', () => {
-  it('should be true', () => {
-    expect(true).toBe(true);
+  const alice = new User({
+    id: 'id-1',
+    password: 'Welcome@123',
+    email: 'alice@gmail.com',
+  });
+
+  let sessionManager: InMemorySessionManager;
+  let useCase: Login;
+
+  beforeEach(() => {
+    sessionManager = new InMemorySessionManager();
+    useCase = new Login(sessionManager);
+  });
+
+  describe('Happy path', () => {
+    it('should create a session with the userId', async () => {
+      const payload = {
+        email: alice.props.email,
+        password: alice.props.password,
+      };
+
+      const { sessionId } = await useCase.execute({
+        ...payload,
+      });
+
+      expect(sessionManager.getSessionSync(sessionId)).toEqual(alice.props.id);
+    });
   });
 });
--- src/users/usecases/login.ts
+++ src/users/usecases/login.ts
@@ -1,5 +1,22 @@
+import { ISessionManager } from '../ports/session-manager.interface';
+
+type Request = {
+  email: string;
+  password: string;
+};
+
+type Response = {
+  sessionId: string;
+};
+
 export class Login {
-  async execute(email: string, password: string) {
-    throw new Error('Not implemented');
+  constructor(private readonly sessionManager: ISessionManager) {}
+
+  async execute(request: Request): Promise<Response> {
+    const sessionId = await this.sessionManager.createSession('id-1');
+
+    return {
+      sessionId,
+    };
   }
 }
