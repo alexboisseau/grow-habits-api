226212f65460dcc948fb4758bcccc3db49bf1e14
alexboisseau <alex.boisseau79@gmail.com>
2023-11-04 16:15:14 +0100

    task: add express-session and passport middlewares in main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './core/app.module';
 
+import * as session from 'express-session';
+import * as passport from 'passport';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: 'GFxlDDu25cdHqLbnxEXxqMnTtgs6eVQdRYUIjyMrYX4=',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 3600000,
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
