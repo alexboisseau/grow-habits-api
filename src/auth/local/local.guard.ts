4f616d546cd5663dbf4d3012542c57faf1500bde
alexboisseau <alex.boisseau79@gmail.com>
2023-11-04 16:15:56 +0100

    task: call login method to create a new session when user logged in

--- src/auth/local/local.guard.ts
+++ src/auth/local/local.guard.ts
@@ -2,4 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalGuard extends AuthGuard('local') {}
+export class LocalGuard extends AuthGuard('local') {
+  constructor() {
+    super();
+  }
+
+  async canActivate(context: any): Promise<boolean> {
+    const result = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    return result;
+  }
+}
