05fb6414441b5f6c4b176120d09b3f0afb8e0317
alexboisseau <alex.boisseau79@gmail.com>
2023-11-05 12:20:24 +0100

    refactor: move login call in a dedicated function

--- src/tests/login.e2e.test.ts
+++ src/tests/login.e2e.test.ts
@@ -3,40 +3,42 @@ import { TestApp } from './utils/test-app';
 import * as request from 'supertest';
 
 describe('Feature: login', () => {
+  async function login(agent: request.SuperAgentTest, payload: any) {
+    return await agent.post('/login').send(payload);
+  }
+
   let app: TestApp;
+  let agent: request.SuperAgentTest;
 
   beforeEach(async () => {
     app = new TestApp();
     await app.setup();
     await app.loadFixtures([e2eUser.alice]);
+    agent = request.agent(app.getHttpServer());
   });
 
   describe('Happy path', () => {
-    it('should return a response with set-cookie header', async () => {
+    it('should logged in', async () => {
       const payload = {
         email: e2eUser.alice.entity.props.email,
         password: 'Welcome@123',
       };
 
-      const result = await request(app.getHttpServer())
-        .post('/login')
-        .send(payload);
+      const result = await login(agent, payload);
 
       expect(result.status).toEqual(201);
       expect(result.header['set-cookie']).toBeDefined();
     });
   });
 
-  describe('Happy path', () => {
-    it('should return a response with set-cookie header', async () => {
+  describe('Unhappy path', () => {
+    it('should fail with wrong credentials', async () => {
       const payload = {
         email: 'fake-email',
         password: 'fake-password',
       };
 
-      const result = await request(app.getHttpServer())
-        .post('/login')
-        .send(payload);
+      const result = await login(agent, payload);
 
       expect(result.status).toEqual(401);
       expect(result.header['set-cookie']).toBeUndefined();
