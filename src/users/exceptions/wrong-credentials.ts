08c45b2308a255159a1a57a0ff32134ac845737e
alexboisseau <alex.boisseau79@gmail.com>
2023-11-07 19:28:12 +0100

    task: remove useless exception

--- src/users/exceptions/wrong-credentials.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestException } from '@nestjs/common';
-
-export class WrongCredentialsException extends BadRequestException {
-  constructor() {
-    super('Invalid credentials');
-  }
-}
