bf28fb66ed2a56c7f69d07a7bca770e978936044
alexboisseau <alex.boisseau79@gmail.com>
2023-11-09 12:34:06 +0100

    task: update user seeds using hash method from bcrypt to hash passwords

--- src/tests/seeds/user-seeds.ts
+++ src/tests/seeds/user-seeds.ts
@@ -1,7 +1,20 @@
-import { userSeeds } from '../../users/tests/user-seeds';
+import { hashSync } from 'bcrypt';
+import { User } from '../../users/entities/user.entity';
 import { UserFixture } from '../fixtures/user-fixture';
 
 export const e2eUsers = {
-  alice: new UserFixture(userSeeds.alice),
-  bob: new UserFixture(userSeeds.bob),
+  alice: new UserFixture(
+    new User({
+      id: 'id-1',
+      email: 'alice@gmail.com',
+      password: hashSync('Welcome@123', 10),
+    }),
+  ),
+  bob: new UserFixture(
+    new User({
+      id: 'id-2',
+      email: 'bob@gmail.com',
+      password: hashSync('azerty', 10),
+    }),
+  ),
 };
