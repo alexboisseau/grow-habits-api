5b4721ccaf29a9f7b76fe6f6a80ea5e504452bee
alexboisseau <alex.boisseau79@gmail.com>
2023-11-10 17:05:23 +0100

    task: add validate method option in config module forRoot method

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HabitModule } from '../habits/habit.module';
 import { UserModule } from '../users/user.module';
 import { AuthModule } from '../auth/auth.module';
+import { validate } from './config/validate';
 
 @Module({
-  imports: [HabitModule, UserModule, AuthModule],
+  imports: [
+    HabitModule,
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
