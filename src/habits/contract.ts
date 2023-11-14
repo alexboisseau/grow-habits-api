fa8c4db05d9f084111e2677af6e4841356b2a4ca
alexboisseau <alex.boisseau79@gmail.com>
2023-11-14 11:43:34 +0100

    task: add min and max constants for habit fields and use them in zod schema

--- src/habits/contract.ts
+++ src/habits/contract.ts
@@ -1,14 +1,35 @@
 import { z } from 'zod';
 import { TrackedHabitStatusArray } from './entities/tracked-habit.entity';
+import {
+  HABIT_CRAVING_MAX_LENGTH,
+  HABIT_CRAVING_MIN_LENGTH,
+  HABIT_CUE_MAX_LENGTH,
+  HABIT_CUE_MIN_LENGTH,
+  HABIT_NAME_MAX_LENGTH,
+  HABIT_NAME_MIN_LENGTH,
+  HABIT_RESPONSE_MAX_LENGTH,
+  HABIT_RESPONSE_MIN_LENGTH,
+  HABIT_REWARD_MAX_LENGTH,
+  HABIT_REWARD_MIN_LENGTH,
+} from './entities/habit.entity';
 
 export namespace HabitAPI {
   export namespace CreateHabitToTrack {
     export const schema = z.object({
-      name: z.string(),
-      cue: z.string(),
-      craving: z.string(),
-      response: z.string(),
-      reward: z.string(),
+      name: z.string().min(HABIT_NAME_MIN_LENGTH).max(HABIT_NAME_MAX_LENGTH),
+      cue: z.string().min(HABIT_CUE_MIN_LENGTH).max(HABIT_CUE_MAX_LENGTH),
+      craving: z
+        .string()
+        .min(HABIT_CRAVING_MIN_LENGTH)
+        .max(HABIT_CRAVING_MAX_LENGTH),
+      response: z
+        .string()
+        .min(HABIT_RESPONSE_MIN_LENGTH)
+        .max(HABIT_RESPONSE_MAX_LENGTH),
+      reward: z
+        .string()
+        .min(HABIT_REWARD_MIN_LENGTH)
+        .max(HABIT_REWARD_MAX_LENGTH),
     });
 
     export type Request = z.infer<typeof schema>;
--- src/habits/entities/habit.entity.ts
+++ src/habits/entities/habit.entity.ts
@@ -12,3 +12,14 @@ export type HabitProps = {
 export class Habit {
   constructor(public props: HabitProps) {}
 }
+
+export const HABIT_NAME_MIN_LENGTH = 3;
+export const HABIT_NAME_MAX_LENGTH = 50;
+export const HABIT_CUE_MIN_LENGTH = 3;
+export const HABIT_CUE_MAX_LENGTH = 255;
+export const HABIT_CRAVING_MIN_LENGTH = 3;
+export const HABIT_CRAVING_MAX_LENGTH = 255;
+export const HABIT_RESPONSE_MIN_LENGTH = 3;
+export const HABIT_RESPONSE_MAX_LENGTH = 255;
+export const HABIT_REWARD_MIN_LENGTH = 3;
+export const HABIT_REWARD_MAX_LENGTH = 255;
