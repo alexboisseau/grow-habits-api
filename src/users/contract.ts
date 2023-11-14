214692f86710ca567cdfb38d995be5cff8bb25b1
alexboisseau <alex.boisseau79@gmail.com>
2023-11-14 11:48:11 +0100

    task: add min and max constants for user fields and use them in zod schema

--- src/users/contract.ts
+++ src/users/contract.ts
@@ -1,11 +1,22 @@
 import { z } from 'zod';
+import {
+  USER_EMAIL_MAX_LENGTH,
+  USER_PASSWORD_MAX_LENGTH,
+  USER_PASSWORD_MIN_LENGTH,
+} from './entities/user.entity';
 
 export namespace UserAPI {
   export namespace Register {
     export const schema = z.object({
-      email: z.string(),
-      password: z.string(),
-      confirmPassword: z.string(),
+      email: z.string().max(USER_EMAIL_MAX_LENGTH),
+      password: z
+        .string()
+        .min(USER_PASSWORD_MIN_LENGTH)
+        .max(USER_PASSWORD_MAX_LENGTH),
+      confirmPassword: z
+        .string()
+        .min(USER_PASSWORD_MIN_LENGTH)
+        .max(USER_PASSWORD_MAX_LENGTH),
     });
 
     export type Request = z.infer<typeof schema>;
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -7,3 +7,7 @@ export type UserProps = {
 export class User {
   constructor(public props: UserProps) {}
 }
+
+export const USER_EMAIL_MAX_LENGTH = 255;
+export const USER_PASSWORD_MIN_LENGTH = 8;
+export const USER_PASSWORD_MAX_LENGTH = 255;
